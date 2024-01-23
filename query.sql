86f7f0dc6b3ff7f876edf3ed72206a76470f9118
dobronravina <90821687+dobronravina@users.noreply.github.com>
2024-01-23 21:58:52 +0100

    Add files via upload

--- query.sql
+++ query.sql
@@ -5,4 +5,4 @@ SELECT book_name from Book where genre = 'Fantasy';
 SELECT book_name from Book where autor_id = 2000003;
 
 --Виводить назви книг, випущених до 2001 року
-SELECT book_name from Book where  first_publication_date < 2000010;
+SELECT book_name from Book where  first_publication_date < 2001;
